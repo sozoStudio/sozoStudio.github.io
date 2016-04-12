0f8beefc241b44cf3887b99d7501d7bf790fef36
alfance <alfance.elric@gmail.com>
2016-04-12 15:16:34 -0400

    kylo first pull

---
+++ app/js/animation.js
@@ -0,0 +1,84 @@
+//intro text rolling.
+var lines = $('.intro-text').text().split("\n");
+var bgm = document.getElementById('bgm1');
+
+var timer,
+    displayLine = function(){
+        var nextLine = lines.shift();
+        if(nextLine){
+            var newLine = $('<li class="line">' + nextLine + '</li>');
+            $('#result').append(newLine);
+            newLine.animate({ 'margin-left':0 }, 1100);
+            timer = setTimeout(displayLine,700);
+        }
+    }
+
+timer = setTimeout(displayLine,400);
+
+//timecard fade in on scroll animation
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+      /* Check the location of each desired element */
+      $('.hideme').each( function(i){
+          var right_of_object = $(this).offset().left + $(this).outerWidth();
+          var right_of_window = $(window).scrollLeft() + $(window).width();
+          /* If the object is completely visible in the window, fade it it */
+          if(right_of_object < right_of_window){
+            $(this).animate({'opacity':'1'},500);
+            setTimeout(
+              function()
+              {$('.timeline-img').fadeIn(500);
+                //do something special
+              }, 5000);
+            //   Timeline nodes and lines positions
+            if($('.timeline-list').length){
+              var heightT = $('.timeline-node').position().top;
+              var heightL = $('.timeline-each').position().top;
+              var node = $('.timeline-node').position().left;
+              var position = $('.timeline-each').position();
+              var positionF = position.left;
+              var width = $('.timeline-each').width();
+              var starting = 150;
+              $('.timeline-list').line(node + 470,heightT,positionF + width,heightL,{color:"#606060", stroke:3, zindex:-1});
+              $('.timeline-list').line(node + 549,heightT,positionF + 499 + width,heightL,{color:"#606060", stroke:3, zindex:-1});
+              $('.timeline-list').line(node + 1110,heightT,positionF + 940 + width,heightL,{color:"#606060", stroke:3, zindex:-1});
+              $('.timeline-list').line(node + 2240,heightT,positionF + 1390 + width,heightL,{color:"#606060", stroke:3, zindex:-1});
+              $('.timeline-list').line(node + 2370,heightT,positionF + 1990 + width,heightL,{color:"#606060", stroke:3, zindex:-1});
+              $('.timeline-list').line(node + 2460,heightT,positionF + 2460 + width,heightL,{color:"#606060", stroke:3, zindex:-1});
+          }
+          }
+          else if( right_of_window > right_of_object ){
+
+              $(this).animate({'opacity':'1'},800);
+          }
+      });
+  });
+
+$( document ).ready(function() {
+    $('html').fadeIn(700);
+    bgm.play();
+    // // $('.timeline-each').css('opacity','0.5');
+    // // $("h1, h2, p").addClass("blue");
+    //  $("div").css("border","3px solid red");
+  var audio1 = document.getElementById("audio1_1");
+  $('body').css('backgroundImage','url("../../img/layout/slide-bg.png")');
+//jquery mouse horizontal scroll
+  (function() {
+  function scrollHorizontally(e) {
+      e = window.event || e;
+      var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+      document.documentElement.scrollLeft -= (delta*20); // Multiplied by 40
+      document.body.scrollLeft -= (delta*20); // Multiplied by 40
+      e.preventDefault();
+  }
+  if (window.addEventListener) {
+      // IE9, Chrome, Safari, Opera
+      window.addEventListener("mousewheel", scrollHorizontally, false);
+      // Firefox
+      window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+  } else {
+      // IE 6/7/8
+      window.attachEvent("onmousewheel", scrollHorizontally);
+  }
+  })();
+});
---
+++ app/js/animation_sunrise.js
@@ -0,0 +1,289 @@
+$(document).ready(function() {
+
+	function getRandomInt(min, max) {
+  		return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	function starGenerate(){
+
+		for (var i = 0;i<100;i++) {
+			var $star;
+			$star = $('<div class="star"></div>');
+			$('.stars').append($star);
+		}
+
+		starAnimate($('.star'));
+	}
+
+	var svgWidth = $('div.landscape').width();
+
+	var svgHeight = $('div.landscape').height();
+
+	function starAnimate(star){
+		star.velocity({
+			scale: function() {
+				return getRandomInt(50,120) + '%';
+			},
+			translateY:
+				function() {
+					return getRandomInt(0,svgHeight/2.5) + 'px';
+				},
+			translateX:
+				function() {
+					return getRandomInt(0,svgWidth) + 'px';
+				},
+			backgroundColorRed: function() {
+				return getRandomInt(70,100) + '%';
+			},
+			backgroundColorGreen: function() {
+				return getRandomInt(70,100) + '%';
+			},
+			backgroundColorBlue: function() {
+				return getRandomInt(70,100) + '%';
+			}
+			},{
+				duration:0,
+			}
+		).velocity({
+			opacity:1
+		},{
+			duration:3000
+		});
+	}
+
+	function observeStars() {
+		$('.tube').velocity({
+			rotateZ: '30deg',
+			rotateX: '-20deg',
+			translateY: '210%'
+		},{
+			duration:0
+		}).velocity({
+			translateY: ['150%', '210%']
+		},{
+			duration:1000
+		}).velocity({
+			rotateZ: ['-40deg','30deg'],
+			rotateX: ['20deg','-20deg']
+		},{
+			duration:3000
+		});
+	}
+
+	setTimeout(function(){
+		observeStars();
+	},5500);
+
+	setTimeout(function(){
+		starGenerate();
+	},6000);
+
+	// var skyGradientTwilight = ['#ffeecc','#e29ae5','#869cee','#509aee','#4988e5'];
+	//
+	// var skyGradientEvening = ['#05257d','#03287d','#02389d','#0247c6','#015cc9'];
+	//
+	// var skyGradientNight = ['#05154d','#030005','#020005','#010005','#010005'];
+
+	var skyGradientNight = ['#fff','#F7DFA4','#aae8f7','#509aee','#4988e5'];
+
+	var skyGradientEvening = ['#05257d','#03287d','#02389d','#0247c6','#015cc9'];
+
+	var skyGradientTwilight = ['#05154d','#030005','#020005','#010005','#010005'];
+
+	$('#SVGID_1_ stop').each(function(index){
+		$(this).velocity({
+			stopColor: skyGradientTwilight[index]
+		},{
+			easing:'easeOutQuart',
+			duration:3000
+		});
+	}).each(function(index){
+		$(this).velocity({
+			stopColor: skyGradientEvening[index]
+		},{
+			easing:'linear',
+			duration:1500
+		});
+	}).each(function(index){
+		$(this).velocity({
+			stopColor: skyGradientNight[index]
+		},{
+			easing:'linear',
+			duration:2000
+		});
+	});
+
+	$('#Hills_in_Distance_1 path').velocity({
+		fill: '#F3E1CF'
+	},{
+		easing:'linear',
+		delay:900,
+		duration:5500
+	});
+
+	$('#Hills_in_Distance_2 path').velocity({
+		fill: '#A7B0B7'
+	},{
+		easing:'linear',
+		delay:900,
+		duration:5500
+	});
+
+    $('#backleft1 path').velocity({
+        fill: '#626458'
+    },{
+        easing:'linear',
+        delay:1400,
+        duration:4500
+    });
+
+    $('#backleft2 path').velocity({
+		fill: '#87897D'
+	},{
+		easing:'linear',
+		delay:1400,
+		duration:4500
+	});
+    $('#backright2 path').velocity({
+		fill: '#6E838E'
+	},{
+		easing:'linear',
+		delay:1900,
+		duration:3550
+	}).velocity({
+		fill: '#868C6D'
+	},{
+		easing:'easeIn',
+		delay:5500,
+		duration:2500
+	}).velocity({
+		fill: '#B2B08A'
+	},{
+		easing:'easeIn',
+		delay:7500,
+		duration:2500
+	});
+
+    $('#backright1 path').velocity({
+		fill: '#4C5F6A'
+	},{
+		easing:'linear',
+		delay:1900,
+		duration:3550
+	}).velocity({
+		fill: '#6E7256'
+	},{
+		easing:'easeIn',
+		delay:5500,
+		duration:2500
+	}).velocity({
+		fill: '#7A7C60'
+	},{
+		easing:'easeIn',
+		delay:7500,
+		duration:2500
+	});
+    $('#backmiddle path').velocity({
+		fill: '#384042'
+	},{
+		easing:'linear',
+		delay:1900,
+		duration:3550
+	});
+    $('#frontleft path').velocity({
+        fill: '#57562E'
+    },{
+        easing:'linear',
+        delay:1900,
+        duration:3550
+    });
+    $('#front path').velocity({
+        fill: '#272D0E'
+    },{
+        easing:'linear',
+        delay:1900,
+        duration:3550
+    });
+
+
+	$('#Distant_Left_Ridge path').velocity({
+		fill: '#19130D'
+	},{
+		easing:'linear',
+		delay:1000,
+		duration:550
+	}).velocity({
+		fill: '#4C3325'
+	},{
+		easing:'easeIn',
+		delay:1500,
+		duration:2500
+	}).velocity({
+		fill: '#593F2A'
+	},{
+		easing:'linear',
+		duration:1000
+	});
+
+	// var sunGradient = ['#FFEE80','#FFC261','#FF5121','#05257d','#005cc9'];
+
+	var sunGradient = ['#FCFFC7','#05257d','#FF5121','#FFC261','#FFEE80'];
+
+	$('#SVGID_2_ stop').velocity({
+		stopColor: sunGradient[2]
+	},{
+		easing:'linear',
+		duration:2250,
+		delay:700
+	}).velocity({
+		stopColor: sunGradient[3]
+	},{
+		easing:'linear',
+		duration:1000
+	}).velocity({
+		stopColor: sunGradient[4]
+	},{
+		easing:'linear',
+		duration:2250
+	}).velocity({
+		stopColor: sunGradient[0]
+	},{
+		easing:'linear',
+		duration:1250
+	});
+
+	//river gradient animations
+	$('#SVGID_3_ stop').velocity({
+		stopColor: '#04182b'
+	},{
+		easing:'linear',
+		duration:2250,
+		delay:700
+	}).velocity({
+		stopColor: '#1e2a35'
+	},{
+		easing:'linear',
+		duration:300,
+	}).velocity({
+		stopColor: '#1e2a35'
+	},{
+		easing:'linear',
+		duration:800,
+		delay:700
+	}).velocity({
+		stopColor: '#233c49'
+	},{
+		easing:'linear',
+		duration:1250,
+		delay:700
+	});
+
+	$('#Sun circle, #SVGID_2_').velocity({
+		cy: '-=400',
+		cx: '+=400'
+	},{
+		easing:'easeOut',
+		duration:14000
+	});
+
+});
