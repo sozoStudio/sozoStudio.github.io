e892658e4f8df4a46e5e0153e79f1ad584902a0c
alfance <[jiao_xue@hotmail.com]>
2016-06-28 17:28:18 -0400

    chrome app

---
+++ FoguangTemple_web/app/js/chap1.js
@@ -0,0 +1,30 @@
+var timelineControllers = angular.module('timelineControllers', ['ui.bootstrap.dialog'])
+.filter('trustUrl', function ($sce) {
+  return function(url) {
+    return $sce.trustAsResourceUrl(url);
+  };
+});
+
+
+///////////////////////////
+//tab controller
+///////////////////////////
+timelineControllers.controller('tabcontrol',function(){
+  $('.chap-title').hide();
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+        var sidebarLen = $('#sidebar-wrapper').width();
+        var sidebar = $('#sidebar-wrapper');
+         console.log ($('#sidebar-wrapper').width());
+        if(sidebarLen < 50){
+          //  $(sidebar).css('border','3px solid red');
+           $(".chap-title").show();
+          $("#sidebar-toggle").removeClass('sidebar-move');
+        }
+        if(sidebarLen > 50){
+          $(".chap-title").hide();
+          $("#sidebar-toggle").addClass('sidebar-move');
+        }
+    });
+});
---
+++ FoguangTemple_web/js/chrome.js
@@ -0,0 +1,8 @@
+var bgm1Statues = 1;
+
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    console.log("sidebar toggle");
+    $("#wrapper-p").toggleClass("toggled");
+});
