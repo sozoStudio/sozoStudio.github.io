6dda773075082a78931e099da72e7fb16f2397e0
shgfl007 <shgfl@hotmail.com>
2016-07-09 21:28:21 -0400

    UPDATE
    auto play added for chap 2

--- FoguangTemple_chromeApp/app/js/audio-chap1.js
+++ FoguangTemple_chromeApp/app/js/audio-chap1.js
@@ -3,8 +3,8 @@ var startbutton = $("#chap2_start, #chap1_start");
 var count = 0;
 var audios = document.getElementsByTagName('audio');
 var getnode = $('.timeline-node');
-
-
+var audios = [];
+var currentAudio = document.getElementById('audio_0');
 function playAudio(direction){
   console.log("start playing audio");
   var nodeactive = $('.timeline-node').css('border-width'),
@@ -16,7 +16,7 @@ function playAudio(direction){
       nodeline = $('.node-line'),
       part1 = $('.timeline-part1'),
       thiscontent = $('.timeline-each').index(this);
-       $(startbutton).fadeOut();
+       //$(startbutton).fadeOut();
   // $(startbutton).click(function(){
   //     var $this = $(this);
   //     audios[0].play();
@@ -41,6 +41,7 @@ function playAudio(direction){
     if (count<audios.length) {
       count++;
       audios[count].load();audios[count].play();
+      currentAudio=audios[count];
     //  scripts for the timeline contents
     // up and down animation for each nodes
         eachnode[count] = $(eachnode[count]).toggleClass('activenode');
@@ -60,7 +61,7 @@ function playAudio(direction){
   }else if (direction==0) {
     //init
     audios[0].load();audios[0].play();
-
+    currentAudio = audios[0];
     //  scripts for the timeline contents
     // up and down animation for each nodes
         eachnode[count] = $(eachnode[count]).toggleClass('activenode');
@@ -81,6 +82,26 @@ function playAudio(direction){
 
 //button change text
 startbutton.text('开始介绍');
+
+startbutton.click(function(){
+
+  audios = document.getElementsByTagName("audio");
+  for (var i = 0; i < audios.length; i++) {
+    console.log("audio " + i + " id is " + audios[i].id);
+    //audios[i]
+  };
+
+  playAudio(0);
+  count = 0;
+  console.log("start button clicked");
+
+
+});
+
+currentAudio.onended = function(){
+  console.log("audio " + count + " ended");
+  playAudio(1);
+};
 
 // $(startbutton).click(function(){
 //     // var $this = $(this);
