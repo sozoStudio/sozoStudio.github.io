b7dc79504522049203f6e3bcadb0cf2d0f2235d3
alfance <[jiao_xue@hotmail.com]>
2016-07-28 09:49:01 -0400

    audio count

--- FoguangTemple_chromeApp/app/js/chrome-storage.js
+++ FoguangTemple_chromeApp/app/js/chrome-storage.js
@@ -6,20 +6,10 @@ var audio2_3 = document.getElementById("2audio_2");
 var audio2_4 = document.getElementById("2audio_3");
 
 var eachAudioCount = 0;
+var newValue;
 
 var chap2Count;
 var currentCount;
-
-// check to see if there is any local storage for the app.
-if (typeof chrome.storage.sync !== 'undefined') {
-    currentCount = 0;
-    chap2Count = 0;
-    chap1Audio = 0;
-} else {
-currentCount = 0;
-chap2Count = 0;
-chap1Audio = 0;
-}
 
 // chapter 2 audios
 var chap2Audio = $(".chap2Audio");
@@ -27,9 +17,15 @@ var chap2Audio = $(".chap2Audio");
 chap2Audio.each(function(i){
   if (i < count_JQ - 1) {
     this.addEventListener("ended", function () {
-          chap2Count = chap2Count + 1;
+        if($(this).hasClass("counted")){
+            newValue = newValue;
+        }
+        else {
+        $(this).addClass("counted");
+          newValue = eachAudioCount ++;
         //   console.log(typeof currentCount );
-          console.log("chap2 count " + chap2Count);
+          console.log("chap2 count " + newValue);
+      }
       });
     }
   });
@@ -69,8 +65,8 @@ window.onload= function() {
     }
 
     document.getElementById('get').onclick = function() {
-        chrome.storage.sync.get('myLine', function(data){
-            console.log(data.myLine);
+        chrome.storage.sync.get('audioEnd', function(data){
+            console.log(data.audioEnd);
         });
     }
 }
